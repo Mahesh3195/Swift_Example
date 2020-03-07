d2a1bd0a8545c002ef7fae2d410b60929110fa1f
MaheshKanike <maheshkanike2@gmail.com>
2020-03-07 15:00:11 +0530

    Implemented Image Control

--- Swift_Example/ViewController.swift
+++ Swift_Example/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("Networking Code here")
+        
+        print("Image Detection")
     }
 
 
